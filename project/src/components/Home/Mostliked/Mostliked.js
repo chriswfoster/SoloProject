375240a387b8813adad174cac8e2acce62d4a564
Chris <chriswfoster@gmail.com>
2017-11-26 22:13:50 -0600

    fade in done on most liked

--- project/src/components/Home/Mostliked/Mostliked.js
+++ project/src/components/Home/Mostliked/Mostliked.js
@@ -9,10 +9,19 @@ import './mostliked.css'
  class Mostliked extends Component{
     constructor(){
         super()
+        this.state= {
+            display: "liked__inner fader"
+        }
      
-        
-    }
+        }
     
+        hiderfunction(){
+        this.state.display === "liked__inner fader" ?
+            this.setState({display: "liked__inner hideit"}) :
+            this.setState({display: "liked__inner fader"})
+        }
+    
+
 
     render(){
        
@@ -20,8 +29,11 @@ import './mostliked.css'
 
      const evenfinder = this.props.allposts.map((dream, i) => (
        testz == dream.likes ?
-        <div className="liked__inner" div key={i}>
+        <div className={this.state.display} div key={i}>
         
+        <label  className="fader__close fader__calign" onClick={() => this.hiderfunction()}>
+                      +
+                    </label>
         
         <center><h1 className="likednittifont"> MOST LIKED RECENT POST! </h1></center>
         <h3><u>{dream.story_title}</u></h3>
