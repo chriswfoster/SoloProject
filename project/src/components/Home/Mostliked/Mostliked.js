24b0688b9606dfad90b970e6a8d31399fdf8ac49
Chris <chriswfoster@gmail.com>
2017-11-26 20:57:49 -0600

    most liked NO fade yet

--- project/src/components/Home/Mostliked/Mostliked.js
+++ project/src/components/Home/Mostliked/Mostliked.js
@@ -5,16 +5,58 @@ import { connect } from "react-redux"
 import Moment from 'react-moment'
 import Comments from '../../Comments/Comments'
 
-import '../home.css'
+import './mostliked.css'
  class Mostliked extends Component{
- 
+    constructor(){
+        super()
+     
+        
+    }
     
 
     render(){
+       
+      const testz = Math.max.apply(Math, (this.props.allposts.map((dream, i) => dream.likes)))
+
+     const evenfinder = this.props.allposts.map((dream, i) => (
+       testz == dream.likes ?
+        <div className="liked__inner" div key={i}>
+        
+        
+        <center><h1 className="likednittifont"> MOST LIKED RECENT POST! </h1></center>
+        <h3><u>{dream.story_title}</u></h3>
+        
+        <div className="likedstorytext" id="likedscrollbar">
+        <pre>
+            {dream.story_text}
+            </pre>
+        </div>
+
+            <div className="likedinfluenceflex">
+            
+                <div className="likedinfluence" id="likedscrollbar">
+                <pre><u>DREAM AID:</u> <br/>{dream.influence}</pre>
+                </div>
+                
+                <div className="likedinfluence" id="likedscrollbar">
+                <pre><u>BACK STORY:</u> <br/>{dream.back_story}</pre>
+                    </div>
+
+                </div>
+
+
+        </div>
+        
+        : ''
+    ))
+
+
+               
+        
         return(
 
             <div>
-                
+                {evenfinder}
                 </div>
 
         )
