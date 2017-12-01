7761e88c8bcb031d35117281b356125857e036ae
Chris Foster <chriswfoster@gmail.com>
2017-12-01 12:41:15 -0600

    fixed some endpoints

--- project/src/components/Home/HomeNav.js
+++ project/src/components/Home/HomeNav.js
@@ -16,10 +16,10 @@ class Home extends Component {
   }
 
   handleLogin() {
-    window.location.href = "http://thawing-falls-40258.herokuapp.com/login"
+    window.location.href = "http://localhost:3000/login"
   }
   handleLogout() {
-    window.location.href = "http://thawing-falls-40258.herokuapp.com/logout"
+    window.location.href = "http://localhost:3000/logout"
   }
 
   render() {
